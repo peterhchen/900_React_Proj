5473f6a4550bf2f8b4e35b016b0ae8555ae29213
peterhchen <peter.hp.chen@gmail.com>
2020-08-06 11:20:21 -0700

    Add files via upload

---
+++ sec11_201_styling-active-route/src/containers/Blog/Blog.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom'
+
+import './Blog.css';
+import Posts from './Posts/Posts'
+import NewPost from './NewPost/NewPost'
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className='Blog'>
+                <header>
+                     <nav>
+                         <ul>
+                             <li><NavLink to="/" exact >Home</NavLink></li>
+                             <li><NavLink to={{
+                                 pathname: '/new-post',
+                                 hash: '#submit',
+                                 search: '?quick-submit=true'
+                             }}>New Post</NavLink></li>
+                         </ul>
+                    </nav>
+                </header>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
