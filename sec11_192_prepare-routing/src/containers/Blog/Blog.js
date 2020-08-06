4af2fccfd8c1c697fecd4a469922a5e8bbd977f5
peterhchen <peter.hp.chen@gmail.com>
2020-08-06 11:17:50 -0700

    Add files via upload

---
+++ sec11_192_prepare-routing/src/containers/Blog/Blog.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Blog.css';
+import Posts from './Posts/Posts'
+class Blog extends Component {
+
+    render () {
+
+        return (
+            <div className='Blog'>
+               <header>
+                     <nav>
+                         <ul>
+                             <li><a href="/">Home</a></li>
+                             <li><a href="/new-post">New Post</a></li>
+                         </ul>
+                    </nav>
+                 </header>
+                {/* <section className="Posts">
+                    {posts}
+                </section> */}
+                <Posts />
+            </div>
+        );
+    }
+}
+
+export default Blog;
